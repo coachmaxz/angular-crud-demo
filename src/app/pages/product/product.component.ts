ff338ccf8160e661820a7b1e868f58bd97d9a05d
Max Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2024-02-09 20:18:51 +0700

    move product lists

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -12,6 +12,14 @@ export class ProductComponent implements OnInit {
 
   title = 'Product';
 
+  productLists: any[] = [
+    { productID: 'PROD00001', productName: 'Product Name 01', editLink: 'product/edit/PROD00001', delLink: 'product/del/PROD00001' },
+    { productID: 'PROD00002', productName: 'Product Name 02', editLink: 'product/edit/PROD00002', delLink: 'product/del/PROD00002' },
+    { productID: 'PROD00003', productName: 'Product Name 03', editLink: 'product/edit/PROD00003', delLink: 'product/del/PROD00003' },
+    { productID: 'PROD00004', productName: 'Product Name 04', editLink: 'product/edit/PROD00004', delLink: 'product/del/PROD00004' },
+    { productID: 'PROD00005', productName: 'Product Name 05', editLink: 'product/edit/PROD00005', delLink: 'product/del/PROD00005' },
+  ]
+
   constructor(private router: Router) {}
   async ngOnInit(): Promise<void> {}
 
