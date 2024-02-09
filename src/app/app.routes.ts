33aa9e7fdc81a0b66d8d8036585eec9a063c3aa0
Max Eakkabin Jaikeawma <eakkabin_it@outlook.co.th>
2024-02-09 20:14:23 +0700

    add route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ProductComponent } from './pages/product/product.component';
+import { ManageAddComponent } from './pages/manage/manage-add.component';
+import { ManageEditComponent } from './pages/manage/manage-edit.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'product', component: ProductComponent },
+  { path: 'product/add', component: ManageAddComponent },
+  { path: 'product/edit/:id', component: ManageEditComponent },
+];
